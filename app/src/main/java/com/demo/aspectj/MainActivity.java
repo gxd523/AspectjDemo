b365ac59764c797297a93fd5a32942e80ee2e904
gxd <guoxiaodong@haqu.com>
2020-02-19 19:02:25 +0800

    upload code

--- app/src/main/java/com/demo/aspectj/MainActivity.java
+++ app/src/main/java/com/demo/aspectj/MainActivity.java
@@ -1,14 +1,24 @@
 package com.demo.aspectj;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @MethodExecutionTime(value = "MainActivity点击事件")
+    public void onClick(View view) {
+        long begin = System.currentTimeMillis();
+        SystemClock.sleep(new Random().nextInt(5000));
+        Log.d("gxd", (System.currentTimeMillis() - begin) + "...");
     }
 }
