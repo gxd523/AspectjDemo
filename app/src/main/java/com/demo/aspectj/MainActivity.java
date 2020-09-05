f3cf2a0777f2a9055e1e3f81a04f3bd4e56fb13c
gxd <guoxiaodong@haqu.com>
2020-09-05 10:04:54 +0800

    no message

--- app/src/main/java/com/demo/aspectj/MainActivity.java
+++ app/src/main/java/com/demo/aspectj/MainActivity.java
@@ -4,19 +4,32 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import java.util.Random;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button button = new Button(this);
+        button.setText("test");
+        button.setTextSize(30);
+        button.setOnClickListener(this);
+        setContentView(button);
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) button.getLayoutParams();
+        params.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        params.gravity = Gravity.CENTER;
     }
 
     @MethodExecutionTime(value = "MainActivity点击事件")
-    public void onClick(View view) {
+    @Override
+    public void onClick(View v) {
         long begin = System.currentTimeMillis();
         SystemClock.sleep(new Random().nextInt(5000));
         Log.d("gxd", (System.currentTimeMillis() - begin) + "...");
--- app/src/main/java/com/demo/aspectj/MethodExecutionTimeAspect.java
+++ app/src/main/java/com/demo/aspectj/MethodExecutionTimeAspect.java
@@ -10,29 +10,33 @@ import org.aspectj.lang.reflect.MethodSignature;
 
 @Aspect
 public class MethodExecutionTimeAspect {
-    //定义切面的规则
-    //1、就再原来的应用中那些注解的地方放到当前切面进行处理
-    //execution（注解名   注解用的地方）
-    @Pointcut("execution(@com.demo.aspectj.MethodExecutionTime *  *(..))")
+    /**
+     * 定义切面的规则
+     * 1、就再原来的应用中那些注解的地方放到当前切面进行处理
+     * execution(注解名 注解用的地方)
+     */
+    @Pointcut("execution(@com.demo.aspectj.MethodExecutionTime * *(..))")
     public void annotationWithMethodExecutionTime() {
     }
 
-
-    //2、对进入切面的内容如何处理
-    //@Before 在切入点之前运行
-//    @After("annotationWithMethodExecutionTime()")
-    //@Around 在切入点前后都运行
+    /**
+     * 2、对进入切面的内容如何处理
+     *
+     * @Before 在切入点之前运行
+     * @After 在切入点之后运行
+     * @Around 在切入点前后都运行
+     */
     @Around("annotationWithMethodExecutionTime()")
     public Object weaveJoinPoint(ProceedingJoinPoint joinPoint) throws Throwable {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         String className = methodSignature.getDeclaringType().getSimpleName();
         String methodName = methodSignature.getName();
         String value = methodSignature.getMethod().getAnnotation(MethodExecutionTime.class).value();
-
-        long begin = System.currentTimeMillis();
+        Log.d("gxd", "value = " + value);
+        long startTime = System.currentTimeMillis();
         Object result = joinPoint.proceed();
-        long duration = System.currentTimeMillis() - begin;
-        Log.d("gxd", String.format("%s.%s()耗时...%dms", className, methodName, duration));
+        long duration = System.currentTimeMillis() - startTime;
+        Log.d("gxd", String.format("%s.%s()...耗时%dms", className, methodName, duration));
         return result;
     }
 }
